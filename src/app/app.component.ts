6fab18f4acd8b14192f4eec9d032f31bb2e12a12
Loic Gnagoh <lgnagoh@yahoo.com>
2022-01-03 09:07:16 +0100

    Adding generic style for CSS button Adding routing in app.module.ts add the url of the backend in environment.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from "./Models/Product.model";
+import {ProductsService} from "./services/products.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecommerce';
+export class AppComponent implements OnInit{
+
+  public Products!: Product[];
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  public getProducts(): void {
+    this.productService.getProducts().subscribe(
+      (response: Product[]) =>{
+        this.Products = response;
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,61 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SingleProductComponent } from './product-list/single-product/single-product.component';
+import { HeaderComponent } from './header/header.component';
+import {AuthService} from "./services/auth.service";
+import {ProductsService} from "./services/products.service";
+import {AuthGuardService} from "./services/auth-guard.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HerosectionComponent } from './herosection/herosection.component';
+import { ShopComponent } from './shop/shop.component';
+
+
+const appRoutes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'home',component: HomeComponent},
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/signin', component: SigninComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'shopping', component: ShopComponent},
+  {path: 'products/view/:id', component: SingleProductComponent},
+  {path: 'about', component: FooterComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ProductListComponent,
+    SingleProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HerosectionComponent,
+    ShopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    ProductsService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {  }
